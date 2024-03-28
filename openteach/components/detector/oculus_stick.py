6eb8cc6a846883bb052fa48318fde3ad1ecb7773
siddhanthaldar <siddhanthaldar14@gmail.com>
2024-03-28 10:30:33 -0400

    Cleaned code for teleop; testing data collection next

--- openteach/components/detector/oculus_stick.py
+++ openteach/components/detector/oculus_stick.py
@@ -1,89 +1,26 @@
-from openteach.constants import VR_FREQ, BIMANUAL_VR_FREQ, ARM_LOW_RESOLUTION, ARM_HIGH_RESOLUTION, ARM_TELEOP_CONT, ARM_TELEOP_STOP , GRIPPER_CLOSE, GRIPPER_OPEN, VR_TCP_HOST, VR_TCP_PORT, VR_CONTROLLER_TOPIC
+from openteach.constants import BIMANUAL_VR_FREQ, VR_TCP_HOST, VR_TCP_PORT, VR_CONTROLLER_TOPIC
 from openteach.components import Component
 from openteach.utils.timer import FrequencyTimer
-from openteach.utils.network import create_pull_socket, create_subscriber_socket, ZMQKeypointPublisher
+from openteach.utils.network import create_subscriber_socket, ZMQKeypointPublisher
 from openteach.components.detector.utils.controller_state import parse_controller_state
 
 # This class is used to detect the hand keypoints from the VR and publish them.            
 class OculusVRStickDetector(Component):
     def __init__(self, 
                  host, 
-                #  oculus_right_port,
-                #  oculus_left_port,
-                #  keypoint_pub_port, 
-                #  button_port, 
-                #  button_publish_port,
                 controller_state_pub_port
     ):
         self.notify_component_start('vr detector')
-        # # Initializing the network socket for getting the raw keypoints
-        # self.raw_keypoint_right_socket = create_pull_socket(host, oculus_right_port)
-        # # Initializing the network socket for resolution button feedback
-        # self.button_keypoint_socket = create_pull_socket(host, button_port)
-        # #self.teleop_reset_socket = create_pull_socket(host, teleop_reset_port)
-        # self.raw_keypoint_left_socket= create_pull_socket(host, oculus_left_port)
 
         # Create a subscriber socket
         self.stick_socket = create_subscriber_socket(VR_TCP_HOST, VR_TCP_PORT, VR_CONTROLLER_TOPIC) #bytes(VR_CONTROLLER_TOPIC, 'utf-8'))
 
-        # ZMQ Keypoint publisher
-        # self.hand_keypoint_publisher = ZMQKeypointPublisher(
-        #     host = host,
-        #     port = keypoint_pub_port
-        # )
-
-        # # Publisher socket for button feedback
-        # self.button_socket_publisher = ZMQKeypointPublisher(
-        #     host = host,
-        #     port = button_publish_port
-        # ) 
+        # Create a publisher for the controller state
         self.controller_state_publisher = ZMQKeypointPublisher(
             host = host,
             port = controller_state_pub_port
         )
-        self.timer = FrequencyTimer(BIMANUAL_VR_FREQ) #VR_FREQ)
-
-
-    # # Function to process the data token received from the VR
-    # def _process_data_token(self, data_token):
-    #     return data_token.decode().strip()
-
-    # # Function to Extract the Keypoints from the String Token sent by the VR
-    # def _extract_data_from_token(self, token):        
-    #     data = self._process_data_token(token)
-    #     information = dict()
-    #     keypoint_vals = [0] if data.startswith('absolute') else [1]
-
-    #     # Data is in the format <hand>:x,y,z|x,y,z|x,y,z
-    #     vector_strings = data.split(':')[1].strip().split('|')
-    #     for vector_str in vector_strings:
-    #         vector_vals = vector_str.split(',')
-    #         for float_str in vector_vals[:3]:
-    #             keypoint_vals.append(float(float_str))
-            
-    #     information['keypoints'] = keypoint_vals
-    #     return information
-
-    # # Function to Publish the right hand transformed Keypoints
-    # def _publish_right_data(self, keypoint_dict):
-    #     self.hand_keypoint_publisher.pub_keypoints(
-    #         keypoint_array = keypoint_dict['keypoints'], 
-    #         topic_name = 'right'
-    #     )
-
-    # # Function to Publish the left hand transformed Keypoints
-    # def _publish_left_data(self, keypoint_dict):
-    #     self.hand_keypoint_publisher.pub_keypoints(
-    #         keypoint_array= keypoint_dict['keypoints'],
-    #         topic_name= 'left'
-    #     )
-
-    # # Function to Publish the Resolution Button Feedback
-    # def _publish_button_data(self,button_feedback):
-    #     self.button_socket_publisher.pub_keypoints(
-    #         keypoint_array = button_feedback, 
-    #         topic_name = 'button'
-    #     )
+        self.timer = FrequencyTimer(BIMANUAL_VR_FREQ)
     
     # Function to Publish the message
     def _publish_controller_state(self, controller_state):
@@ -97,43 +34,22 @@ class OculusVRStickDetector(Component):
 
         print("oculus stick stream")
         while True:
-            # try:
+            try:
                 self.timer.start_loop()
 
-                # Getting the raw keypoints
-                # raw_right_keypoints = self.raw_keypoint_right_socket.recv()
-                # raw_left_keypoints = self.raw_keypoint_left_socket.recv()
-                # button_feedback = self.button_keypoint_socket.recv()
-                # print("getting oculus message")
                 message = self.stick_socket.recv_string()
-                # print("oculus message", message)
-                if message == "oculus_controller": #VR_CONTROLLER_TOPIC:
+                if message == "oculus_controller":
                     continue
 
                 controller_state = parse_controller_state(message)
 
                 # Publish message
-                # self._publish_controller_state("hello")
                 self._publish_controller_state(controller_state)
 
-                # if button_feedback==b'Low':
-                #     button_feedback_num = ARM_LOW_RESOLUTION
-                # else:
-                #     button_feedback_num = ARM_HIGH_RESOLUTION
-
-                # # Processing the keypoints and publishing them
-                # keypoint_right_dict = self._extract_data_from_token(raw_right_keypoints)
-                # keypoint_left_dict = self._extract_data_from_token(raw_left_keypoints)
-                # self._publish_right_data(keypoint_right_dict)
-                # self._publish_left_data(keypoint_left_dict)
-                # self._publish_button_data(button_feedback_num)
                 self.timer.end_loop()
 
-            # except KeyboardInterrupt:
-            #     break
+            except KeyboardInterrupt:
+                break
 
         self.controller_state_publisher.stop()
-        # self.raw_keypoint_right_socket.close()
-        # self.raw_keypoint_left_socket.close()
-        # self.hand_keypoint_publisher.stop()
         print('Stopping the oculus keypoint extraction process.')
--- openteach/components/operators/bimanual_stick.py
+++ openteach/components/operators/bimanual_stick.py
@@ -1,12 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import zmq
 import time
 
-from mpl_toolkits.mplot3d import Axes3D
-from tqdm import tqdm
-
-from copy import deepcopy as copy
 from openteach.constants import *
 from openteach.utils.timer import FrequencyTimer
 from openteach.utils.network import ZMQKeypointSubscriber , ZMQKeypointPublisher
@@ -66,9 +60,6 @@ class BimanualArmOperator(Operator):
         self,
         host, 
         controller_state_port,
-        # transformed_keypoints_port,
-        # use_filter=False,
-        # arm_resolution_port = None, 
         gripper_port=None,
         cartesian_publisher_port = None,
         joint_publisher_port = None,
@@ -84,30 +75,12 @@ class BimanualArmOperator(Operator):
         )
 
         # # Subscribers for the transformed hand keypoints
-        # self._transformed_arm_keypoint_subscriber = ZMQKeypointSubscriber(
-        #     host=host,
-        #     port=transformed_keypoints_port,
-        #     topic='transformed_hand_frame'
-        # )
-        # # Subscribers for the transformed arm frame
-        # self._transformed_hand_keypoint_subscriber = ZMQKeypointSubscriber(
-        #     host=host,
-        #     port=transformed_keypoints_port,
-        #     topic='transformed_hand_coords'
-        # )
         self._transformed_arm_keypoint_subscriber = None
         self._transformed_hand_keypoint_subscriber = None
 
         # Initalizing the robot controller
         self._robot = Bimanual(ip=RIGHT_ARM_IP)
         self.robot.reset()
-        
-        # # Initialize the subscribers for the arm resolution , gripper and saving the data
-        # self._arm_resolution_subscriber = ZMQKeypointSubscriber(
-        #     host= host,
-        #     port= arm_resolution_port,
-        #     topic = 'button'
-        # )
 
         # Gripper and cartesian publisher
         self.gripper_publisher = ZMQKeypointPublisher(
@@ -134,12 +107,6 @@ class BimanualArmOperator(Operator):
         home_pose=np.array(self.robot.get_cartesian_position())
         self.robot_init_H = self.robot_pose_aa_to_affine(home_pose)
         self._timer = FrequencyTimer(BIMANUAL_VR_FREQ)
-
-        # # Use the filter
-        # self.use_filter = use_filter
-        # if use_filter:
-        #     robot_init_cart = self._homo2cart(self.robot_init_H)
-        #     self.comp_filter = Filter(robot_init_cart, comp_ratio=0.8)
 
         # Class Variables
         self.resolution_scale =1
@@ -209,132 +176,6 @@ class BimanualArmOperator(Operator):
         rotation = R.from_matrix(affine[:3, :3]).as_rotvec()
         return np.concatenate([translation, rotation])
 
-    # # Get the hand frame
-    # def _get_hand_frame(self):
-    #     for i in range(10):
-    #         data = self.transformed_arm_keypoint_subscriber.recv_keypoints(flags=zmq.NOBLOCK)
-    #         if data is not None:
-    #             break 
-    #     if data is None:
-    #         return None
-    #     return np.asanyarray(data).reshape(4, 3)
-    
-    # # Get the resolution scale mode
-    # def _get_resolution_scale_mode(self):
-    #     data = self._arm_resolution_subscriber.recv_keypoints()
-    #     res_scale = np.asanyarray(data).reshape(1)[0] # Make sure this data is one dimensional
-    #     return res_scale
-    
-    # # Get the arm teleop state from the hand keypoints
-    # def _get_arm_teleop_state_from_hand_keypoints(self):
-    #     pause_state ,pause_status,pause_left =self.get_pause_state_from_hand_keypoints()
-    #     pause_status =np.asanyarray(pause_status).reshape(1)[0] 
-    #     return pause_state,pause_status,pause_left
-
-    # # Convert frame to homogeneous matrix
-    # def _turn_frame_to_homo_mat(self, frame):
-    #     t = frame[0]
-    #     R = frame[1:]
-
-    #     homo_mat = np.zeros((4, 4))
-    #     homo_mat[:3, :3] = np.transpose(R)
-    #     homo_mat[:3, 3] = t
-    #     homo_mat[3, 3] = 1
-
-    #     return homo_mat
-
-    # # Convert homogeneous matrix to cartesian coords (position vector+axis angles)
-    # def _homo2cart(self, homo_mat):
-    #     # Here we will use the resolution scale to set the translation resolution
-    #     t = homo_mat[:3, 3]
-    #     R = Rotation.from_matrix(
-    #         homo_mat[:3, :3]).as_rotvec(degrees=False)
-
-    #     cart = np.concatenate(
-    #         [t, R], axis=0
-    #     )
-
-    #     return cart
-    
-    # # Get the scaled cartesian position
-    # def _get_scaled_cart_pose(self, moving_robot_homo_mat):
-    #     # Get the cart pose without the scaling
-    #     unscaled_cart_pose = self._homo2cart(moving_robot_homo_mat)
-
-    #     # Get the current cart pose
-    #     home_pose = self.robot.get_cartesian_position()
-    #     home_pose_array = np.array(home_pose)  # Convert tuple to numpy array
-    #     current_homo_mat = self.robot_pose_aa_to_affine(home_pose_array)
-    #     current_cart_pose = self._homo2cart(current_homo_mat)
-
-    #     # Get the difference in translation between these two cart poses
-    #     diff_in_translation = unscaled_cart_pose[:3] - current_cart_pose[:3]
-    #     scaled_diff_in_translation = diff_in_translation * self.resolution_scale
-        
-    #     scaled_cart_pose = np.zeros(6)
-    #     scaled_cart_pose[3:] = unscaled_cart_pose[3:] # Get the rotation directly
-    #     scaled_cart_pose[:3] = current_cart_pose[:3] + scaled_diff_in_translation # Get the scaled translation only
-
-    #     return scaled_cart_pose
-
-    # # Function to reset the teleoperation
-    # def _reset_teleop(self):
-    #     # Just updates the beginning position of the arm
-    #     print('****** RESETTING TELEOP ****** ')
-    #     first_hand_frame = self._get_hand_frame()
-    #     while first_hand_frame is None:
-    #         first_hand_frame = self._get_hand_frame()
-
-    #     self.hand_init_H = self._turn_frame_to_homo_mat(first_hand_frame)
-    #     self.hand_init_t = copy(self.hand_init_H[:3, 3])
-
-    #     self.is_first_frame = False
-    #     home_pose = self.robot.get_cartesian_position()
-    #     home_pose_array = np.array(home_pose)  # Convert tuple to numpy array
-    #     self.robot_init_H = self.robot_pose_aa_to_affine(home_pose_array)
-
-    #     return first_hand_frame
-    
-    # # Toggle gripper state using pinky finger pinch
-    # def get_gripper_state_from_hand_keypoints(self):
-    #     transformed_hand_coords= self._transformed_hand_keypoint_subscriber.recv_keypoints()
-    #     distance = np.linalg.norm(transformed_hand_coords[OCULUS_JOINTS['pinky'][-1]]- transformed_hand_coords[OCULUS_JOINTS['thumb'][-1]])
-    #     thresh = 0.04
-    #     gripper_fr =False
-    #     if distance < thresh:
-    #         self.gripper_cnt+=1
-    #         if self.gripper_cnt==1:
-    #             self.prev_gripper_flag = self.gripper_flag
-    #             self.gripper_flag = not self.gripper_flag 
-    #             gripper_fr=True
-    #     else: 
-    #         self.gripper_cnt=0
-    #     gripper_state = np.asanyarray(self.gripper_flag).reshape(1)[0]
-    #     status= False  
-    #     if gripper_state!= self.prev_gripper_flag:
-    #         status= True
-    #     return gripper_state , status , gripper_fr     
-
-    # # Toggle the robot to pause/resume using ring/middle finger pinch, both finger modes are supported to avoid any hand pose noise issue
-    # def get_pause_state_from_hand_keypoints(self):
-    #     transformed_hand_coords= self._transformed_hand_keypoint_subscriber.recv_keypoints()
-    #     ring_distance = np.linalg.norm(transformed_hand_coords[OCULUS_JOINTS['ring'][-1]]- transformed_hand_coords[OCULUS_JOINTS['thumb'][-1]])
-    #     middle_distance = np.linalg.norm(transformed_hand_coords[OCULUS_JOINTS['middle'][-1]]- transformed_hand_coords[OCULUS_JOINTS['thumb'][-1]])
-    #     thresh = 0.04
-    #     pause_left= True
-    #     if ring_distance < thresh or middle_distance < thresh:
-    #         self.pause_cnt+=1
-    #         if self.pause_cnt==1:
-    #             self.prev_pause_flag=self.pause_flag
-    #             self.pause_flag = not self.pause_flag       
-    #     else:
-    #         self.pause_cnt=0
-    #     pause_state = np.asanyarray(self.pause_flag).reshape(1)[0]
-    #     pause_status= False  
-    #     if pause_state!= self.prev_pause_flag:
-    #         pause_status= True 
-    #     return pause_state , pause_status , pause_left
-
     # Apply retargeted angles to the robot
     def _apply_retargeted_angles(self, log=False):
        
@@ -376,19 +217,13 @@ class BimanualArmOperator(Operator):
         if gripper_state is not None and gripper_state != self.gripper_correct_state:
             self.robot.set_gripper_state(gripper_state * 800)
             self.gripper_correct_state = gripper_state
-
-        # Apply the relative transform
-        # home_pose = self.robot._controller.robot.get_position_aa()[1]
             
         if self.start_teleop:
-            # home_affine = self.robot_pose_aa_to_affine(home_pose) #   # translation in m
             home_translation = self.home_affine[:3, 3]
             home_rotation = self.home_affine[:3, :3]
 
             # Target
-            # home_translation = self.init_affine[:3, 3]
             target_translation = home_translation + relative_affine[:3, 3]
-            # home_rotation = self.init_affine[:3, :3]
             target_rotation = home_rotation @ relative_affine[:3, :3]
             
             target_affine = np.block([[target_rotation, target_translation.reshape(-1,1)], [0, 0, 0, 1]])
@@ -396,9 +231,6 @@ class BimanualArmOperator(Operator):
             # If this target pose is too far from the current pose, move it to the closest point on the boundary.
             target_pose = self.affine_to_robot_pose_aa(target_affine).tolist()
             current_pose = self.robot._controller.robot.get_position_aa()[1]
-            # current_pose = self.robot.get_position_aa()[1]
-            # delta_translation = np.array(
-            #     target_pose[:3]) - np.array(current_pose[:3])
             delta_translation = np.array(
                     target_pose[:3] - np.array(current_pose[:3]))
             
@@ -408,7 +240,6 @@ class BimanualArmOperator(Operator):
                                         a_max=ROBOT_SERVO_MODE_STEP_LIMITS[1])
 
             # a_min and a_max are the boundaries of the robot's workspace; clip absolute position to these boundaries.
-
             des_translation = delta_translation + np.array(current_pose[:3])
             des_translation = np.clip(des_translation,
                                         a_min=ROBOT_WORKSPACE[0],
@@ -429,101 +260,3 @@ class BimanualArmOperator(Operator):
 
         if self.start_teleop:
             self.robot.arm_control(des_pose)
-
-            # ret_code, (joint_pos, joint_vels,
-            #             joint_effort) = self.robot.get_joint_states()
-            # # # # Populate all records for state.
-            # current_state_action_pair = self.robot.get_current_state_action_tuple(
-            #     # just use time.time? why use the last sent msg ts? Will this help get an exact state
-            #     ts=last_sent_msg_ts,
-            #     pose_aa=current_pose,
-            #     joint_angles=joint_pos,
-            #     gripper_state=gripper_flag,
-            #     des_pose=des_pose,
-            #     controller_ts=move_msg.created_timestamp,
-            #     last_sent_ts=last_sent_msg_ts  # t-1 actually;
-            # )
-
-            # env_state_action_df = pd.concat(
-            #     [env_state_action_df, current_state_action_pair.to_df()], ignore_index=True)
-
-        # # Get the new arm teleop state
-        # new_arm_teleop_state,pause_status,pause_left = self._get_arm_teleop_state_from_hand_keypoints()
-        # if self.is_first_frame or (self.arm_teleop_state == ARM_TELEOP_STOP and new_arm_teleop_state == ARM_TELEOP_CONT):
-        #     moving_hand_frame = self._reset_teleop() 
-            
-        # else:
-        #     moving_hand_frame = self._get_hand_frame()
-        # self.arm_teleop_state = new_arm_teleop_state
-        # arm_teleoperation_scale_mode = self._get_resolution_scale_mode()
-        # if arm_teleoperation_scale_mode == ARM_HIGH_RESOLUTION:
-        #     self.resolution_scale = 1
-        # elif arm_teleoperation_scale_mode == ARM_LOW_RESOLUTION:
-        #     self.resolution_scale = 0.6
-        # if moving_hand_frame is None: 
-        #     return # It means we are not on the arm mode yet instead of blocking it is directly returning
-        
-        # self.hand_moving_H = self._turn_frame_to_homo_mat(moving_hand_frame)
-
-        # # Transformation code
-        # H_HI_HH = copy(self.hand_init_H) # Homo matrix that takes P_HI to P_HH - Point in Inital Hand Frame to Point in Home Hand Frame
-        # H_HT_HH = copy(self.hand_moving_H) # Homo matrix that takes P_HT to P_HH
-        # H_RI_RH = copy(self.robot_init_H) # Homo matrix that takes P_RI to P_RH
-      
-        # # Transformation from initial hand frame to moving hand frame
-        # H_HT_HI = np.linalg.pinv(H_HI_HH) @ H_HT_HH
-        
-        # # Here there are two matrices because the rotation is asymmetric and we imagine waitwe are holding the endeffector and moving the robot.
-        # H_R_V= np.array([[0 , 0, 1, 0], 
-        #                 [0 , 1, 0, 0],
-        #                 [-1, 0, 0, 0],
-        #                 [0, 0 ,0 , 1]])
-        # # The translation is completely symmetric and mimics your hand movement and we imagine we are holding the endeffector and moving the robot.
-        # H_T_V = np.array([[0, 0 ,-1, 0],
-        #                  [0 ,-1, 0, 0],
-        #                  [-1, 0, 0, 0],
-        #                 [0, 0, 0, 1]])
-
-        # H_HT_HI_r=(pinv(H_R_V)@H_HT_HI@H_R_V)[:3,:3]
-        # H_HT_HI_t=(pinv(H_T_V)@H_HT_HI@H_T_V)[:3,3]_is_first_frame
-        
-        # #Calculate relative affine matrix 
-        # relative_affine = np.block(
-        # [[ H_HT_HI_r,  H_HT_HI_t.reshape(3, 1)], [0, 0, 0, 1]])
-
-        # # Vector addition of trannslation coomponents
-        # target_translation = H_RI_RH[:3,3] + relative_affine[:3,3]
-
-        # # Vector multiplication of rotation components to find the new rotation matrix
-        # target_rotation = H_RI_RH[:3, :3] @ relative_affine[:3,:3]
-        # # New pose matrix
-        # H_RT_RH = np.block(
-        #             [[target_rotation, target_translation.reshape(-1, 1)], [0, 0, 0, 1]])
-        # self.robot_moving_H = copy(H_RT_RH)
-
-        # final_pose = self._get_scaled_cart_pose(self.robot_moving_H)
-        # final_pose[0:3]=final_pose[0:3]*1000
-
-        # if self.use_filter:
-        #     final_pose = self.comp_filter(final_pose)
-       
-        # gripper_state,status_change, gripper_flag =self.get_gripper_state_from_hand_keypoints()
-        # if self.gripper_cnt==1 and status_change is True:
-        #     self.gripper_correct_state= gripper_state
-        #     self.robot.set_gripper_state(self.gripper_correct_state*800)
-
-        # # We save the states here during teleoperation as saving directly at 90Hz seems to be too fast for XArm.
-        # self.gripper_publisher.pub_keypoints(self.gripper_correct_state,"gripper_left")
-        # position=self.robot.get_cartesian_position()
-        # joint_position= self.robot.get_joint_position()
-        # self.cartesian_publisher.pub_keypoints(position,"cartesian")
-        # self.joint_publisher.pub_keypoints(joint_position,"joint")
-        # self.cartesian_command_publisher.pub_keypoints(final_pose,"cartesian")
-    
-        # if self.arm_teleop_state == ARM_TELEOP_CONT and gripper_flag == False:
-        #     self.robot.arm_control(final_pose)
-
-       
-        
-
-
