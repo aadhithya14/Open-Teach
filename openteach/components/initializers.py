a4f96cba6edeb4c76c058fcedd3721fa4ebe8911
BobbyPeng123 <zp2019@nyu.edu>
2024-04-05 09:32:42 -0400

    add

--- openteach/components/initializers.py
+++ openteach/components/initializers.py
@@ -85,6 +85,37 @@ class FishEyeCameras(ProcessInstantiator):
                 args = (cam_idx, )
             ))
 
+class EmeetCameras(ProcessInstantiator):
+    """
+    Returns all the camera processes. Start the list of processes to start
+    the camera stream.
+    """
+    def __init__(self, configs):
+        super().__init__(configs)
+        # Creating all the camera processes
+        self._init_camera_processes()
+
+    def _start_component(self, cam_idx):
+        component = EmeetCamera(
+            stream_configs = dict(
+                host = self.configs.host_address,
+                port = self.configs.cam_port_offset + cam_idx
+            ),
+            cam_serial_num = self.configs.robot_cam_serial_numbers[cam_idx],
+            cam_id = cam_idx + 1,
+            cam_configs = self.configs.cam_configs,
+            stream_oculus = True if self.configs.oculus_cam == cam_idx else False
+        )
+        component.stream()
+
+    def _init_camera_processes(self):
+        for cam_idx in range(len(self.configs.robot_cam_serial_numbers)):
+            self.processes.append(Process(
+                target = self._start_component,
+                args = (cam_idx, )
+            ))
+
+
 
 class TeleOperator(ProcessInstantiator):
     """
--- openteach/components/sensors/__init__.py
+++ openteach/components/sensors/__init__.py
@@ -1,3 +1,4 @@
 from .realsense import RealsenseCamera
 from .fish_eye_cam import FishEyeCamera
+from .emeet import EmeetCamera
 #from .xela import XelaCurvedSensors
